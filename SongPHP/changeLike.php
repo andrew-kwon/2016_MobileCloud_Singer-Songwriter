966b14d025f0e53c017cb3717a1692fbf4361b51
Young Hoon Kwon <yhkk2585@naver.com>
2016-05-23 09:14:51 +0900

    Modify LikeListView php file to select double

---
+++ SongPHP/changeLike.php
@@ -0,0 +1,38 @@
+
+<?php
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASS','485197');
+	define('DB','songLikeDB');
+	define('SongDB','singersongBoard');
+	
+	$likeUser=$_GET['UserID']; 
+	$con = mysqli_connect(HOST,USER,PASS,DB);
+ 
+
+	$sql = "select SongName, UserID from songLike WHERE likeUserID='$likeUser'";
+	$number=1;
+
+	$result = mysqli_query($con,$sql); 
+	$boardCon=mysqli_connect(HOST,USER,PASS,SongDB);	
+	while($row = mysqli_fetch_row($result))
+	{
+		$songSql="select * from songDB WHERE songName='$row[0]' and userID='$row[1]'";
+		$songResult=mysqli_query($boardCon,$songSql);
+		while($songRow=mysqli_fetch_row($songResult))
+		{
+			
+			echo ':::'.$songRow[1];
+			echo ':::'.$songRow[2];
+			echo ':::'.$songRow[3];
+			echo ':::'.$songRow[4];
+			echo ':::'.$songRow[5];
+			echo ':::'.$songRow[6];
+			echo ':::'.$songRow[7];
+			echo '--:--';
+		}
+		
+	}
+ 	mysqli_close($boardCon);
+	mysqli_close($con);
+?>
